2294fdc639eec25a98889c67c54dd8b8bef05c1f
Ricardo Taipe <rdavid28@hotmail.es>
2022-10-03 16:30:44 -0500

    refactor store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
 import { favoriteRecipesReducer } from "../features/favoriteRecipes/favoriteRecipesSlice.js";
 import { searchTermReducer } from "../features/searchTerm/searchTermSlice.js";
 import { allRecipesReducer } from "../features/allRecipes/allRecipesSlice.js";
 
-export const store = createStore(
-  combineReducers({
+export default configureStore({
+  reducer: {
     favoriteRecipes: favoriteRecipesReducer,
     searchTerm: searchTermReducer,
     allRecipes: allRecipesReducer,
-  })
-);
+  },
+});
