d6b4fa98418a65422f2bda711e0d9685db424940
Ricardo Taipe <rdavid28@hotmail.es>
2022-10-05 14:28:33 -0500

    refactor search slice

--- src/features/searchTerm/searchTermSlice.js
+++ src/features/searchTerm/searchTermSlice.js
@@ -4,24 +4,18 @@ import { createSlice } from "@reduxjs/toolkit";
 ///////////////////////////////////////
 
 export const searchSlice = createSlice({
-  name: "searchTerm",
-  initialState: {
-    searchTerm: "",
-  },
+  name: "search",
+  initialState: "",
   reducers: {
-    setSearchTerm: (state, action) => {
-      state.searchTerm = action.payload;
-    },
-    clearSearchTerm: (state) => {
-      state.searchTerm = "";
-    },
+    setSearchTerm: (state, action) => (state = action.payload),
+    clearSearchTerm: (state) => (state = ""),
   },
 });
 
 // Selectors
 ///////////////////////////////////////
 
-export const selectSearchTerm = (state) => state.searchTerm.searchTerm;
+export const selectSearchTerm = (state) => state.search;
 
 // Exports
 ///////////////////////////////////////
